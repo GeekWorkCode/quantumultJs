6b4071ca3c60be5adb90cacf6e61e46ce63a655b
zsCool <asd486@live.com>
2022-02-26 23:52:05 +0800

    Update newword.js

--- newword.js
+++ newword.js
@@ -1,3 +1,8 @@
 let app = JSON.parse($request.body);
-app.course_state.queues.new = 49;
+if(app.course.queues.exercises == 0) {
+app.course.exercises == 50;
+};
+if(app.course.queues.new == 0) {
+app.course.queues.new == 49;
+};
 $done({ body: JSON.stringify(app) });
