5bc4d2496e510c56de20073ab3d1b0dbb460e2e6
zsCool <asd486@live.com>
2022-03-01 00:04:59 +0800

    Update Wordevent.js

--- Wordevent.js
+++ Wordevent.js
@@ -7,8 +7,8 @@ if (url.indexOf(path1) != -1) {
     for (var i = 0; i < obj.events.length; i++) {
       if (obj.events[i].data.screen) {
         
-          obj.events[i].data.screen = "guess";
-          obj.events[i].data.action = "open";
+          obj.events[i].data.screen = "tutor-view";
+          obj.events[i].data.action = "collapse";
         
       }
     }
