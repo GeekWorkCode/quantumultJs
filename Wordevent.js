b9c15f20e1e1e862c976ee4e318fc79730518edc
zhangshuai <asd486@live.com>
2022-03-04 20:48:18 +0800

    修改判断

--- Wordevent.js
+++ Wordevent.js
@@ -5,11 +5,9 @@ if (url.indexOf(path1) != -1) {
   var obj = JSON.parse(body);
   if (obj.events) {
     for (var i = 0; i < obj.events.length; i++) {
-      if (obj.events[i].data.screen) {
-        if (obj.events[i].data.screen != "login") {
-          obj.events[i].data.screen = "tutor-view";
-          obj.events[i].data.action = "collapse";
-        }
+      if (obj.events[i].data.screen && obj.events[i].data.screen != "login") {
+        obj.events[i].data.screen = "tutor-view";
+        obj.events[i].data.action = "collapse";
       }
     }
     body = JSON.stringify(obj);
