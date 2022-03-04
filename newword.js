28ac190e54ca1a69d446fb6f6396ce33737ee606
zhangshuai <asd486@live.com>
2022-03-04 22:02:27 +0800

    修改

--- newword.js
+++ newword.js
@@ -1,31 +1,31 @@
 let app = JSON.parse($request.body);
 
-if (app.course_state){
+if (app.course_state) {
 
-var bookmark = JSON.parse(app.course_state.bookmark);
+    var bookmark = JSON.parse(app.course_state.bookmark);
 
-if(app.course_state.queues.new == 0) {
-app.course_state.queues.new = 39;
-};
+    // if (app.course_state.queues.new == 0) {
+    app.course_state.queues.new = 50;
+    // };
 
-if(app.course_state.queues.exercises == 0) {
-app.course_state.queues.exercises = 328;
-};
+    // if (app.course_state.queues.exercises == 0) {
+    app.course_state.queues.exercises = 139;
+    // };
 
-if(app.course_state.queues.repeats_waiting == 0) {
-app.course_state.queues.repeats_waiting = 39;
-};
+    // if (app.course_state.queues.repeats_waiting == 0) {
+    //     app.course_state.queues.repeats_waiting = 39;
+    // };
 
 
     if (bookmark.questions.new_unit_sn == -1) {
         bookmark.questions.new_unit_sn = 1034;
         bookmark.questions.variation_new_unit_sns = { '356933e0-836e-4603-82ac-86e692c9b0c0': 1034 };
-        bookmark.exercises = { modification_sn: 298, linguistica360_removed: true };
+        bookmark.exercises = { modification_sn: 139, linguistica360_removed: true };
     };
     bookmark.subscription_limited = false;
     bookmark = JSON.stringify(bookmark);
     app.course_state.bookmark = bookmark;
 
 }
- 
+
 $done({ body: JSON.stringify(app) });
