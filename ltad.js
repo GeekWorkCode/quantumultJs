39af6e73114a886ea4f684a57e8f7a5f84bcec5a
zsCool <asd486@live.com>
2022-11-10 05:00:56 +0800

    更新 ltad.js
    推送

--- ltad.js
+++ ltad.js
@@ -1,6 +1,7 @@
 // ^https?://m\.client\.10010\.com/uniAdmsInterface/getHomePageAd url script-response-body ltad.js
 const url = $request.url
 var body = $response.body
+//test推送
 const path1 = "/uniAdmsInterface/getHomePageAd"
 if (url.indexOf(path1) != -1) {
     let obj = JSON.parse(body);
